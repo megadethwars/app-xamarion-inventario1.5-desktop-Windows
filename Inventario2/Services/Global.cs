2234ba0d764d2b73a043fdd033abd33532741c37
omar9703 <42915871+omar9703@users.noreply.github.com>
2020-04-29 11:10:22 -0500

    detalles de titulos

--- Inventario2/Services/Global.cs
+++ Inventario2/Services/Global.cs
@@ -6,8 +6,8 @@ namespace Inventario2.Services
 {
     public class Global
     {
-        //public static string url = "http://avsinvservice.azurewebsites.net/";
-        public static string url = "http://127.0.0.1:5000/";
+        public static string url = "http://avsinvservice.azurewebsites.net/";
+        //public static string url = "http://127.0.0.1:5000/";
         public static string keystorege = "wO8R0xJGc9+VleJHkKEL2AHLmZOEUvLcZg0M1KaMNI2lB9Jd27SShyHhlgeCGEQLOs7SCgYffIx4OI6TBABFPg==";
     }
 }
