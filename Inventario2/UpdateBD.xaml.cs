7ef8e23733ab8cb43498175fd3ea08fff9d101ba
omar9703 <42915871+omar9703@users.noreply.github.com>
2020-05-17 15:35:41 -0500

    requerimientos excel

--- Inventario2/UpdateBD.xaml.cs
+++ Inventario2/UpdateBD.xaml.cs
@@ -37,174 +37,183 @@ namespace Inventario2
 
         private async void Button_Clicked_1(object sender, EventArgs e)
         {
-            isStopped = false;
-            if (f != null)
+            try
             {
-                
-
-                
-                ExcelEngine excelEngine = new ExcelEngine();
-                IApplication application = excelEngine.Excel;
-                application.DefaultVersion = ExcelVersion.Excel2013;
 
 
-                IWorkbook workbook = application.Workbooks.Open(f.GetStream());
-
-                //Access first worksheet from the workbook.
-                IWorksheet worksheet = workbook.Worksheets[1];
-
-                int y = 2;
-                while (!worksheet.GetValueRowCol(y,3).Equals("") && worksheet.GetValueRowCol(y, 2) != null)
+                isStopped = false;
+                if (f != null)
                 {
-                    y++;
-                    
-                }
-                //porcent.Text = y.ToString();
-                //Filas.
-                int row = 2;
-                y = y - 1;
-                bool t = await DisplayAlert("Advertencia", "Este archivo contiene " + y.ToString() + "productos, Desea Continuar?", "Si", "No");
-                while (!worksheet.GetValueRowCol(row, 2).Equals("") && worksheet.GetValueRowCol(row, 2)!= null)
-                {
-                    
 
-                    if (isStopped)
+
+
+                    ExcelEngine excelEngine = new ExcelEngine();
+                    IApplication application = excelEngine.Excel;
+                    application.DefaultVersion = ExcelVersion.Excel2013;
+
+
+                    IWorkbook workbook = application.Workbooks.Open(f.GetStream());
+
+                    //Access first worksheet from the workbook.
+                    IWorksheet worksheet = workbook.Worksheets[1];
+
+                    int y = 2;
+                    while (!worksheet.GetValueRowCol(y, 3).Equals("") && worksheet.GetValueRowCol(y, 2) != null)
                     {
-                        porcent.Text = "Detenido";
-                        break;
+                        y++;
+
                     }
-                    
-                    if (t == false)
-                        break;
-                    //columnas
-
-                    var strs = worksheet.GetText(row, 1);
-
-                    //list = await App.MobileService.GetTable<InventDB>().Where(u => u.codigo == strs).ToListAsync();
-
-
-                    var devices = await DeviceService.getdevicebycode(strs);
-                    if (devices == null)
+                    //porcent.Text = y.ToString();
+                    //Filas.
+                    int row = 2;
+                    y = y - 1;
+                    bool t = await DisplayAlert("Advertencia", "Este archivo contiene " + y.ToString() + "productos, Desea Continuar?", "Si", "No");
+                    while (!worksheet.GetValueRowCol(row, 2).Equals("") && worksheet.GetValueRowCol(row, 2) != null)
                     {
 
-                        await DisplayAlert("Buscando", "error de conexion con el servidor", "OK");
-                        porcent.Text = "Error de conexion";
-                        break;
-                    }
 
-                    if (devices[0].statuscode == 500)
-                    {
-                        await DisplayAlert("Buscando", "error interno del servidor", "OK");
-                        porcent.Text = "Error de sincronizacion";
-                        break;
-                    }
+                        if (isStopped)
+                        {
+                            porcent.Text = "Detenido";
+                            break;
+                        }
 
-                    if (devices[0].statuscode == 404)
-                    {
-                        //await DisplayAlert("Buscando", "producto no encontrado", "OK");
+                        if (t == false)
+                            break;
+                        //columnas
 
-                        //var id = Guid.NewGuid().ToString();
-                        ModelDevice n = new ModelDevice
+                        var strs = worksheet.GetText(row, 1);
+
+                        //list = await App.MobileService.GetTable<InventDB>().Where(u => u.codigo == strs).ToListAsync();
+
+
+                        var devices = await DeviceService.getdevicebycode(strs);
+                        if (devices == null)
                         {
 
-                            codigo = strs,
-                            serie = (string)worksheet.GetValueRowCol(row, 2),
-                            compra = worksheet.GetText(row, 8),
-                            costo = worksheet.GetText(row, 6),
-                            descompostura = worksheet.GetText(row, 11),
-                            marca = worksheet.GetText(row, 4),
-                            modelo = worksheet.GetText(row, 5),
-                            producto = worksheet.GetText(row, 3),
-                            observaciones = worksheet.GetText(row, 9),
-                            origen = worksheet.GetText(row, 7),
-                            pertenece = worksheet.GetText(row, 10),
-                            //ID = id,
-                            IDlugar = 1,
-                            
-                        };
+                            await DisplayAlert("Buscando", "error de conexion con el servidor", "OK");
+                            porcent.Text = "Error de conexion";
+                            break;
+                        }
 
-                        n.serie = n.serie.Replace('\x22', '\0');
-
-                        await DeviceService.postdevice(JsonConvert.SerializeObject(n));
-                        //await App.MobileService.GetTable<InventDB>().InsertAsync(n);
-
-
-                    }
-
-                    if (devices[0].statuscode == 200 || devices[0].statuscode == 201)
-                    {
-                        devices[0].compra = worksheet.GetText(row, 8);
-                        devices[0].costo = worksheet.GetText(row, 6);
-                        devices[0].descompostura = worksheet.GetText(row, 11);
-                        devices[0].marca = worksheet.GetText(row, 4);
-                        devices[0].modelo = worksheet.GetText(row, 5);
-                        devices[0].producto = worksheet.GetText(row, 3);
-                        devices[0].observaciones = worksheet.GetText(row, 9);
-                        devices[0].origen = worksheet.GetText(row, 7);
-                        devices[0].pertenece = worksheet.GetText(row, 10);
-                        devices[0].serie = (string)worksheet.GetValueRowCol(row, 2);
-                        devices[0].IDlugar = 1;
-                        devices[0].serie = devices[0].serie.Replace('\x22', '\0');
-                        await DeviceService.putdevice(devices[0].ID, JsonConvert.SerializeObject(devices[0]));
-                        //await App.MobileService.GetTable<InventDB>().UpdateAsync(list[0]);
-                    }
-
-
-
-                    porcent.Text = worksheet.GetText(row, 1) + "  " + (row * 100 / (y)).ToString() + "%";
-                    row = row + 1;
-                }
-                row = 2;
-                porcent.Text = "terminado";
-
-                /*
-                for (int x = 2; x < 1577; x++)
-                {
-                    //columnas
-
-                    var strs = worksheet.GetText(x, 1);
-                    list = await App.MobileService.GetTable<InventDB>().Where(u => u.codigo == strs).ToListAsync();
-                    if (list.Count != 0)
-                    {
-                        list[0].compra = worksheet.GetText(x, 8);
-                        list[0].costo = worksheet.GetText(x, 6);
-                        list[0].descompostura = worksheet.GetText(x, 11);
-                        list[0].marca = worksheet.GetText(x, 4);
-                        list[0].modelo = worksheet.GetText(x, 5);
-                        list[0].nombre = worksheet.GetText(x, 3);
-                        list[0].observaciones = worksheet.GetText(x, 9);
-                        list[0].origen = worksheet.GetText(x, 7);
-                        list[0].pertenece = worksheet.GetText(x, 10);
-                        list[0].lugar = "Almacen";
-                        list[0].serie = worksheet.GetText(x, 1);
-                        await App.MobileService.GetTable<InventDB>().UpdateAsync(list[0]);
-                    }
-                    else
-                    {
-                        var id = Guid.NewGuid().ToString();
-                        InventDB n = new InventDB
+                        if (devices[0].statuscode == 500)
                         {
+                            await DisplayAlert("Buscando", "error interno del servidor", "OK");
+                            porcent.Text = "Error de sincronizacion";
+                            break;
+                        }
 
-                            codigo = strs,
-                            serie = worksheet.GetText(x, 1),
-                            compra = worksheet.GetText(x, 8),
-                            costo = worksheet.GetText(x, 6),
-                            descompostura = worksheet.GetText(x, 11),
-                            marca = worksheet.GetText(x, 4),
-                            modelo = worksheet.GetText(x, 5),
-                            nombre = worksheet.GetText(x, 3),
-                            observaciones = worksheet.GetText(x, 9),
-                            origen = worksheet.GetText(x, 7),
-                            pertenece = worksheet.GetText(x, 10),
-                            ID = id,
-                            lugar = "Almacen"
-                        };
-                        await App.MobileService.GetTable<InventDB>().InsertAsync(n);
+                        if (devices[0].statuscode == 404)
+                        {
+                            //await DisplayAlert("Buscando", "producto no encontrado", "OK");
+
+                            //var id = Guid.NewGuid().ToString();
+                            ModelDevice n = new ModelDevice
+                            {
+
+                                codigo = strs,
+                                serie = (string)worksheet.GetValueRowCol(row, 2),
+                                compra = worksheet.GetText(row, 8),
+                                costo = worksheet.GetText(row, 6),
+                                descompostura = worksheet.GetText(row, 11),
+                                marca = worksheet.GetText(row, 4),
+                                modelo = worksheet.GetText(row, 5),
+                                producto = worksheet.GetText(row, 3),
+                                observaciones = worksheet.GetText(row, 9),
+                                origen = worksheet.GetText(row, 7),
+                                pertenece = worksheet.GetText(row, 10),
+                                //ID = id,
+                                IDlugar = 1,
+
+                            };
+
+                            n.serie = n.serie.Replace('\x22', '\0');
+
+                            await DeviceService.postdevice(JsonConvert.SerializeObject(n));
+                            //await App.MobileService.GetTable<InventDB>().InsertAsync(n);
+
+
+                        }
+
+                        if (devices[0].statuscode == 200 || devices[0].statuscode == 201)
+                        {
+                            devices[0].compra = worksheet.GetText(row, 8);
+                            devices[0].costo = worksheet.GetText(row, 6);
+                            devices[0].descompostura = worksheet.GetText(row, 11);
+                            devices[0].marca = worksheet.GetText(row, 4);
+                            devices[0].modelo = worksheet.GetText(row, 5);
+                            devices[0].producto = worksheet.GetText(row, 3);
+                            devices[0].observaciones = worksheet.GetText(row, 9);
+                            devices[0].origen = worksheet.GetText(row, 7);
+                            devices[0].pertenece = worksheet.GetText(row, 10);
+                            devices[0].serie = (string)worksheet.GetValueRowCol(row, 2);
+                            devices[0].IDlugar = 1;
+                            devices[0].serie = devices[0].serie.Replace('\x22', '\0');
+                            await DeviceService.putdevice(devices[0].ID, JsonConvert.SerializeObject(devices[0]));
+                            //await App.MobileService.GetTable<InventDB>().UpdateAsync(list[0]);
+                        }
+
+
+
+                        porcent.Text = worksheet.GetText(row, 1) + "  " + (row * 100 / (y)).ToString() + "%";
+                        row = row + 1;
                     }
-                    porcent.Text = worksheet.GetText(x, 1)+"  "+(x * 100 / 1576).ToString()+"%";
+                    row = 2;
+                    porcent.Text = "terminado";
 
-                }*/
-                workbook.Close();
+                    /*
+                    for (int x = 2; x < 1577; x++)
+                    {
+                        //columnas
+
+                        var strs = worksheet.GetText(x, 1);
+                        list = await App.MobileService.GetTable<InventDB>().Where(u => u.codigo == strs).ToListAsync();
+                        if (list.Count != 0)
+                        {
+                            list[0].compra = worksheet.GetText(x, 8);
+                            list[0].costo = worksheet.GetText(x, 6);
+                            list[0].descompostura = worksheet.GetText(x, 11);
+                            list[0].marca = worksheet.GetText(x, 4);
+                            list[0].modelo = worksheet.GetText(x, 5);
+                            list[0].nombre = worksheet.GetText(x, 3);
+                            list[0].observaciones = worksheet.GetText(x, 9);
+                            list[0].origen = worksheet.GetText(x, 7);
+                            list[0].pertenece = worksheet.GetText(x, 10);
+                            list[0].lugar = "Almacen";
+                            list[0].serie = worksheet.GetText(x, 1);
+                            await App.MobileService.GetTable<InventDB>().UpdateAsync(list[0]);
+                        }
+                        else
+                        {
+                            var id = Guid.NewGuid().ToString();
+                            InventDB n = new InventDB
+                            {
+
+                                codigo = strs,
+                                serie = worksheet.GetText(x, 1),
+                                compra = worksheet.GetText(x, 8),
+                                costo = worksheet.GetText(x, 6),
+                                descompostura = worksheet.GetText(x, 11),
+                                marca = worksheet.GetText(x, 4),
+                                modelo = worksheet.GetText(x, 5),
+                                nombre = worksheet.GetText(x, 3),
+                                observaciones = worksheet.GetText(x, 9),
+                                origen = worksheet.GetText(x, 7),
+                                pertenece = worksheet.GetText(x, 10),
+                                ID = id,
+                                lugar = "Almacen"
+                            };
+                            await App.MobileService.GetTable<InventDB>().InsertAsync(n);
+                        }
+                        porcent.Text = worksheet.GetText(x, 1)+"  "+(x * 100 / 1576).ToString()+"%";
+
+                    }*/
+                    workbook.Close();
+                }
+            }
+            catch(ArgumentException exc)
+            {
+                DisplayAlert("Error", "El archivo esta con un mal formato, revise el formato\ny consulte el manual para mas informacion", "Aceptar");
             }
         }
     }
